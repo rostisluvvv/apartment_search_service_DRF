020d51bd3278902f1d4f36b50909ebd0ff9c8fb6
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-04-07 13:55:08 +0300

    serializers ApartmentInfo

---
+++ apartment_service/apartments/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import ApartmentInfo
+
+
+class ApartmentInfoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ApartmentInfo
+        fields = ('title',
+                  'landlord',
+                  'favorites',
+                  'city',
+                  'address',
+                  'apartment_area',
+                  'price',
+                  'published')
