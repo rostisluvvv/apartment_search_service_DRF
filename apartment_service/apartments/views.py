8afb3e4e35e55691664f117f144d5d871a0a796f
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-04-07 16:50:46 +0300

    add permissions

--- apartment_service/apartments/views.py
+++ apartment_service/apartments/views.py
@@ -1,12 +1,12 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from .permissions import IsOwnerOrReadOnly
+from .permissions import IsLandlordOrReadOnly
 from .serializers import ApartmentInfoSerializer
 from .models import ApartmentInfo
 
 
 class ApartmentInfoViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticated, IsLandlordOrReadOnly]
     queryset = ApartmentInfo.objects.all()
     serializer_class = ApartmentInfoSerializer
--- apartment_service/profiles/views.py
+++ apartment_service/profiles/views.py
@@ -1,17 +1,18 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
+from profiles.permissions import IsServiceUserOrReadOnly
 from profiles.models import ServiceUser, Favorites
 from profiles.serializers import ServiceUserSerializer, FavoritesSerializer
 
 
 class ServiceUserViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdminUser]
     queryset = ServiceUser.objects.all()
     serializer_class = ServiceUserSerializer
 
 
 class FavoritesViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsServiceUserOrReadOnly]
     queryset = Favorites.objects.all()
     serializer_class = FavoritesSerializer
