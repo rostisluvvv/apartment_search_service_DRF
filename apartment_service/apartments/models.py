5ff10190bac84922a21a448a94cb8c7812c5c663
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-04-07 11:58:13 +0300

    add model

---
+++ apartment_service/apartments/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+from profiles.models import ServiceUser
+
+
+class ApartmentInfo(models.Model):
+    landlord = models.ForeignKey(ServiceUser,
+                                 on_delete=models.CASCADE,
+                                 related_name='apartments')
+    title = models.CharField(max_length=150)
+    content = models.TextField(max_length=500, null=True, blank=True)
+    photo = models.ImageField(upload_to='uploads/%Y/%m/%d/', null=True)
+    city = models.CharField(max_length=250)
+    address = models.CharField(max_length=100)
+    apartment_area = models.FloatField(max_length=30)
+    price = models.FloatField(max_length=30)
+    published = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-published']
+
+
