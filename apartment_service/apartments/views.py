6af438b9495fceb5ae7a82456ee5f192f5023b77
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-04-07 13:55:40 +0300

    add views

--- apartment_service/apartments/views.py
+++ apartment_service/apartments/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .permissions import IsOwnerOrReadOnly
+from .serializers import ApartmentInfoSerializer
+from .models import ApartmentInfo
+
+
+class ApartmentInfoViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    queryset = ApartmentInfo.objects.all()
+    serializer_class = ApartmentInfoSerializer
--- apartment_service/profiles/views.py
+++ apartment_service/profiles/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from profiles.models import ServiceUser, Favorites
+from profiles.serializers import ServiceUserSerializer, FavoritesSerializer
+
+
+class ServiceUserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = ServiceUser.objects.all()
+    serializer_class = ServiceUserSerializer
+
+
+class FavoritesViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Favorites.objects.all()
+    serializer_class = FavoritesSerializer
