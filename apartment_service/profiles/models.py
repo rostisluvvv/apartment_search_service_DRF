7637eec37ece89e71daa119fab950896445d1f3d
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-04-07 16:51:17 +0300

    change fields in Favorites

--- apartment_service/profiles/models.py
+++ apartment_service/profiles/models.py
@@ -14,19 +14,23 @@ class ServiceUser(AbstractUser):
     avatar = models.ImageField(upload_to='user/avatar/',
                                blank=True,
                                null=True)
-    middle_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, blank=True, null=True)
     phone_number = models.CharField(max_length=14)
-    favorites = models.OneToOneField('Favorites',
-                                     on_delete=models.CASCADE,
-                                     related_name='service_users')
 
     def __str__(self):
         return self.username
 
 
 class Favorites(models.Model):
-    city = models.CharField(max_length=250)
-    apartment_area_gte = models.FloatField(max_length=30)
-    apartment_area_lte = models.FloatField(max_length=30)
-    price_gte = models.FloatField(max_length=30)
-    price_lte = models.FloatField(max_length=30)
+    service_user = models.ForeignKey(ServiceUser,
+                                     related_name='favorites',
+                                     on_delete=models.CASCADE)
+    city = models.CharField(max_length=250, blank=True, null=True)
+    apartment_area_gte = models.FloatField(max_length=30,
+                                           blank=True,
+                                           null=True)
+    apartment_area_lte = models.FloatField(max_length=30,
+                                           blank=True,
+                                           null=True)
+    price_gte = models.FloatField(max_length=30, blank=True, null=True)
+    price_lte = models.FloatField(max_length=30, blank=True, null=True)
