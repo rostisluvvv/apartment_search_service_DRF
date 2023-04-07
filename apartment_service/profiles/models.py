7d1b0207413a10bd7eed9234ec2cf733161bd9e8
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-04-07 11:58:48 +0300

    small fix

--- apartment_service/profiles/models.py
+++ apartment_service/profiles/models.py
@@ -15,13 +15,18 @@ class ServiceUser(AbstractUser):
                                blank=True,
                                null=True)
     middle_name = models.CharField(max_length=50)
-    phone_number = models.IntegerField(max_length=13)
-    favorites = models.ManyToManyField('Favorites',
-                                       related_name='service_users')
+    phone_number = models.CharField(max_length=14)
+    favorites = models.OneToOneField('Favorites',
+                                     on_delete=models.CASCADE,
+                                     related_name='service_users')
 
     def __str__(self):
         return self.username
 
 
 class Favorites(models.Model):
-    pass
+    city = models.CharField(max_length=250)
+    apartment_area_gte = models.FloatField(max_length=30)
+    apartment_area_lte = models.FloatField(max_length=30)
+    price_gte = models.FloatField(max_length=30)
+    price_lte = models.FloatField(max_length=30)
