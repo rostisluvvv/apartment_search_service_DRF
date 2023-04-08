2cb4fd08cf3079f3015d735414eba77c6725040d
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-04-08 16:01:11 +0300

    add swagger

--- apartment_service/config/urls.py
+++ apartment_service/config/urls.py
@@ -1,14 +1,31 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
 
+from rest_framework import permissions
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Apartment Search Service API",
+      default_version='v1',
+
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+
+
     path('admin/', admin.site.urls),
     path('api/', include('profiles.urls')),
-    path('api/', include('apartments.urls'))
-
+    path('api/', include('apartments.urls')),
+    re_path(r'api/v1/swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/v1/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/v1/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ]
 
 
